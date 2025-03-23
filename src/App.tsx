8c658eaa710ad2cc3d4fa776a35d561d11b32d90
Debabrata Karfa <im@deb.im>
2025-03-23 11:30:18 +0530

    feat: First Commit

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { useCallback, useEffect, useState } from '@lynx-js/react'
+
+import './App.css'
+import arrow from './assets/arrow.png'
+import lynxLogo from './assets/lynx-logo.png'
+import reactLynxLogo from './assets/react-logo.png'
+
+export function App() {
+  const [alterLogo, setAlterLogo] = useState(false)
+
+  useEffect(() => {
+    console.info('Hello, ReactLynx')
+  }, [])
+
+  const onTap = useCallback(() => {
+    'background only'
+    setAlterLogo(!alterLogo)
+  }, [alterLogo])
+
+  const onButtonClick = useCallback(() => {
+    console.info('Button clicked!')
+  }, [])
+
+  return (
+    <view>
+      <view className='Background' />
+      <view className='App'>
+        <view className='Banner'>
+          <view className='Logo' bindtap={onTap}>
+            {alterLogo
+              ? <image src={reactLynxLogo} className='Logo--react' />
+              : <image src={lynxLogo} className='Logo--lynx' />}
+          </view>
+          <text className='Title'>React</text>
+          <text className='Subtitle'>on Lynx</text>
+        </view>
+        <view className='Content'>
+          <image src={arrow} className='Arrow' />
+          <text className='Description'>Tap the logo and have fun!</text>
+          <text className='Hint'>
+            Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>
+            to see updates here.
+          </text>
+          <view className='MyButton' bindtap={onButtonClick}>
+            <text>Click Me</text>
+          </view>
+        </view>
+        <view className='Footer'>
+          <text className='FooterText'>Powered by Avancerv</text>
+          <image src={lynxLogo} className='FooterLogo' /> 
+        </view>
+      </view>
+    </view>
+  )
+}
